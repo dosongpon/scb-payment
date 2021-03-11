fc20796a60520d4713bf96242cd8607f909fa9bb
SPP <songpon.ph@gmail.com>
2021-03-11 13:49:31 +0700

    update url

--- scb_payment/constants.py
+++ scb_payment/constants.py
@@ -3,8 +3,9 @@ A libraly that provides a python interface to SCB Payment API
 """
 
 ENDPOINTS = {
-    "API_ROOT": "https://api-sandbox.partners.scb/partners/sandbox", #DEV
+    # "API_ROOT": "https://api-sandbox.partners.scb/partners/sandbox",  # DEV
     # "API_ROOT": "https://api-uat.partners.scb/partners", #UAT
+    "API_ROOT": "https://api.partners.scb/partners",  # Production
     "OAUTH_AUTHORIZE_PATH": "/v2/oauth/authorize",
     "OAUTH_TOKEN_PATH": "/v1/oauth/token",
     "OAUTH_TOKEN_REFRESH_PATH": "/v2/oauth/authorize",
